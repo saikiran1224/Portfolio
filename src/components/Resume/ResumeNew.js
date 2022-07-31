f2fd623bd72a6c87cc24bc749e861815cd4999e7
saikiran1224 <knvrssaikiran@gmail.com>
2022-07-31 21:37:02 +0530

    Updated README.md file

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -9,7 +9,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/soumyajit4419/portfolio/master/src/Assets/Soumyajit_Behera-BIT_MESRA.pdf";
+  "https://raw.githubusercontent.com/saikiran1224/Portfolio/master/src/Assets/K.N.V.R.S.Sai%20Kiran%20-%20Resume%20-%2030072022.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
